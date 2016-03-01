a0cc693f29859347e9200aab37042fcdfe53834e
Max-Hu <huzhiwise@gmail.com>
2016-03-01 18:01:11 +0800

    implement translate task

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -12,3 +12,64 @@ function splitInput(inputs){
   }
   return splitArr;
 }
+
+function translateStr(splitArr,dictionary){
+  var translatedArr = [];
+  splitArr.forEach( function(numberStr){
+    var thirdBit = getThirdBit(numberStr,dictionary);
+    var restBit = getLastTwoBits(numberStr,dictionary);
+    var translatedElement = '';
+    if (thirdBit && restBit) {
+      translatedElement = thirdBit + ' and ' + restBit;
+    }
+
+    if (thirdBit && !restBit){
+      translatedElement = thirdBit;
+    }
+
+    if (!thirdBit && restBit){
+      translatedElement = restBit;
+    }
+    translatedArr.push(translatedElement);
+  });
+  return translatedArr;
+}
+
+function getThirdBit(numberStr,dictionary){
+  var thirdBit;
+  if (numberStr.length > 2){
+    var thirdStr = numberStr.substr(0,1);
+    if (thirdStr != '0'){
+      thirdBit = dictionary[parseInt(thirdStr)] + ' hundred';
+    }
+  }
+  return thirdBit ;
+}
+
+function getLastTwoBits(numberStr,dictionary){
+  var lastTwoBits;
+  var lastTwoNumber;
+  if (numberStr.length < 3){
+    lastTwoNumber = numberStr;
+  } else {
+    if (numberStr.substr(1,1) === "0"){
+      lastTwoNumber = parseInt(numberStr.substr(2));
+    } else {
+      lastTwoNumber = parseInt(numberStr.substr(1,2));
+    }
+  }
+
+
+  if (lastTwoNumber){
+    lastTwoBits = dictionary[parseInt(lastTwoNumber)];
+    if (!lastTwoBits){
+      var secondNumber = parseInt(lastTwoNumber.toString().substr(0,1))*10;
+      var secondBit = dictionary[secondNumber];
+      var firstNumber = parseInt(lastTwoNumber.toString().substr(1));
+      var firstBit = dictionary[firstNumber];
+      lastTwoBits = secondBit + ' ' + firstBit;
+    }
+  }
+  return lastTwoBits;
+}
+
